f16b4a27da76694d0117342f5c69223c7c8baab9
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-09-07 14:53:34 +0000

    Deploy XiaocongDong/XiaocongDong.github.io to github.com/XiaocongDong/XiaocongDong.github.io.git:master

---
+++ opensource/simple/build/precache-manifest.f79533d0aa00ddadaa6b3b2aa080198b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcc363f271fe26e0cecd16191082efd2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef02ad608fde5083c10d",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "70c454e08688e58d3d99",
+    "url": "/static/js/2.671dc04c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.671dc04c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef02ad608fde5083c10d",
+    "url": "/static/js/main.c8a8fea4.chunk.js"
+  },
+  {
+    "revision": "4b4be035672f5f8bc943",
+    "url": "/static/js/runtime-main.6f26bf25.js"
+  }
+]);
